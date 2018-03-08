ba4f482a5d9602204b508b4c18ac43fcc6909c94
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2018-03-08 10:19:59 +0100

    init commit

---
+++ app/src/main/java/com/vidicom/fakeblackredale/FakeSmallRecognitionActivity.kt
@@ -0,0 +1,12 @@
+package com.vidicom.fakeblackredale
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class FakeSmallRecognitionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fake_small_recognition)
+    }
+}
---
+++ app/src/main/java/com/vidicom/fakeblackredale/StartActivityWithAR.kt
@@ -0,0 +1,45 @@
+package com.vidicom.fakeblackredale
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_start_with_ar.*
+
+class StartActivityWithAR : AppCompatActivity() {
+
+    companion object {
+        val SMILE = 12
+        val NO_SMILE = 13
+    }
+    private  val requestCode = 123;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_with_ar)
+
+        stopsTalkingButton.setOnClickListener {
+            Intent(this,FakeSmallRecognitionActivity::class.java).let {
+                this.startActivityForResult(it,requestCode)
+            }
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == this.requestCode){
+            if(resultCode == SMILE){
+                text.text = "SMILE detected, is doing now what is supposed to be when there is a smile "
+                stopsTalkingButton.text = "Finish test project"
+                stopsTalkingButton.setOnClickListener {
+                    finish()
+                }
+            }else if(resultCode == NO_SMILE){
+                text.text = "SMILE NOT detected, is doing now what is supposed to be when there is a smile "
+                stopsTalkingButton.text = "Finish test project"
+                stopsTalkingButton.setOnClickListener {
+                    finish()
+                }
+            }
+        }
+    }
+}
